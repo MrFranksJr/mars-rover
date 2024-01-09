3e7717a0eed71e5e2e6a115f3a93d30807be8d83
Franky Jr Blondeel <frankyjr@me.com>
2024-01-09 14:52:04 +0100

    empty command handled

--- src/main/java/io/tripled/marsrover/ConsolePresenter.java
+++ src/main/java/io/tripled/marsrover/ConsolePresenter.java
@@ -2,7 +2,13 @@ package io.tripled.marsrover;
 
 public class ConsolePresenter implements MessagePresenter {
     @Override
-    public void welcomeMessage() { System.out.println("**************************\n" + "**      Mars Rover      **\n" + "**************************\n" + "> q to quit"); }
+    public void welcomeMessage() {
+        System.out.println("**************************");
+        System.out.println("**      Mars Rover      **");
+        System.out.println("**************************");
+        System.out.println("Determine the maxCoordinate of the simulation by setting the maximum coordinate [0-100]");
+        System.out.println("> q to quit");
+    }
 
     @Override
     public void unknownCommand(String input) {
@@ -11,7 +17,12 @@ public class ConsolePresenter implements MessagePresenter {
 
     @Override
     public void printCommand() {
-        System.out.println("Printed!");
+        System.out.println("***************************************************************************************************************************************************");
+        System.out.println("*   Print state of simulation     | {state}                                                   | ex: state                                         *");
+        System.out.println("*   Land a new rover              | {land {x} {y}}                                            | ex: land 1 5                                      *");
+        System.out.println("*   Quit the application          | {Q}                                                                                                           *");
+        System.out.println("*   Print API overview            | {P}                                                                                                           *");
+        System.out.println("***************************************************************************************************************************************************");
     }
 }
 
