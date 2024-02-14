70c3b5becbb9ffd4d59d26fe9fcb44912758c74f
Franky Jr Blondeel <frankyjr@me.com>
2024-02-14 11:54:19 +0100

    small bugfix in direction recognition

--- business/src/main/java/io/tripled/marsrover/business/domain/rover/Direction.java
+++ business/src/main/java/io/tripled/marsrover/business/domain/rover/Direction.java
@@ -4,7 +4,7 @@ public enum Direction {
     FORWARD, LEFT, RIGHT, BACKWARD;
 
     public static Direction convertTextToDirection(String directionInText) {
-        return switch (directionInText) {
+        return switch (directionInText.toLowerCase()) {
             case "f" -> FORWARD;
             case "b" -> BACKWARD;
             case "l" -> LEFT;
