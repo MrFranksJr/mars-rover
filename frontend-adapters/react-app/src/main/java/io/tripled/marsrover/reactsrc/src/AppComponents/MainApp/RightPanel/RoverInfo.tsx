95ce0477739bfa7dac95951c15936d5c7adf28fc
Franky Jr Blondeel <frankyjr@me.com>
2024-03-25 15:23:53 +0100

    roverdata flowing to simulationstate window

---
+++ frontend-adapters/react-app/src/main/java/io/tripled/marsrover/reactsrc/src/AppComponents/MainApp/RightPanel/RoverInfo.tsx
@@ -0,0 +1,18 @@
+import {Rover} from "../../../interfaces.ts";
+
+function RoverInfo(rover: Rover) {
+
+    return (
+        <>
+            <div>
+                <h4>Rover {rover.roverName}:</h4>
+                Position: ({rover.roverXPosition} - {rover.roverYPosition})<br/>
+                Heading: {rover.roverHeading}<br/>
+                Hitpoints: {rover.hitPoints}/5<br/>
+                Status: {rover.operationalStatus}<br/>
+            </div>
+        </>
+    )
+}
+
+export default RoverInfo
--- frontend-adapters/react-app/src/main/java/io/tripled/marsrover/reactsrc/src/AppComponents/MainApp/RightPanel/SimulationState.tsx
+++ frontend-adapters/react-app/src/main/java/io/tripled/marsrover/reactsrc/src/AppComponents/MainApp/RightPanel/SimulationState.tsx
@@ -1,8 +1,11 @@
 import {useContext} from "react";
 import {SimulationContext} from "../../SimulationContext.tsx";
+import RoverInfo from "./RoverInfo.tsx";
 
 function SimulationState() {
-    const {simulation} = useContext(SimulationContext)
+    const {simulation} = useContext(SimulationContext);
+    const roversInSimulation = simulation?.roverList;
+
 
     return (
         <>
@@ -11,7 +14,20 @@ function SimulationState() {
                 <p>The Simulation size is {simulation?.simulationSize}</p>
                 <p>The Simulation has {simulation?.totalCoordinates} total Coordinates</p>
                 <hr/>
-                <div className="rover-states">ROVERS HERE</div>
+                {roversInSimulation?.length === 0 ? (<div className="rover-states">There are currently no Rovers in the Simulation</div>)
+                    :
+                    (roversInSimulation?.map(rover => (
+                        <RoverInfo
+                            key={rover.roverName}
+                            roverName={rover.roverName}
+                            roverHeading={rover.roverHeading}
+                            roverXPosition={rover.roverXPosition}
+                            roverYPosition={rover.roverYPosition}
+                            hitPoints={rover.hitPoints}
+                            operationalStatus={rover.operationalStatus}
+                        />
+                        ))
+                )}
             </div>
         </>
     )
