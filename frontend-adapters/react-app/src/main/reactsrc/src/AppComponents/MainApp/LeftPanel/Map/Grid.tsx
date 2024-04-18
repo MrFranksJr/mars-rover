93cc585a969f0c66f4c7555845eea4b655034c47
Franky Jr Blondeel <frankyjr@me.com>
2024-04-18 17:47:29 +0200

    render rover cells on new dynamic grid

--- frontend-adapters/react-app/src/main/reactsrc/src/AppComponents/MainApp/LeftPanel/Map/Grid.tsx
+++ frontend-adapters/react-app/src/main/reactsrc/src/AppComponents/MainApp/LeftPanel/Map/Grid.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '../../../../styles/Map.css';
+import {SimulationContext} from "../../../SimulationContext.tsx";
 
 interface GridProps {
-    arrOfObj: number[];
+    simulationSize: number;
 }
 
-const Grid: React.FC<GridProps> = (props) => {
-    const { arrOfObj } = props;
+const Grid: React.FC<GridProps> = () => {
+    const {simulation} = useContext(SimulationContext);
+
+    console.log(simulation.totalCoordinates)
+    const array = new Array(simulation.totalCoordinates).fill(0).map((_, index) => index);
 
     // Calculate the number of columns and rows based on the square root of the array length
-    const numOfCols = Math.ceil(Math.sqrt(arrOfObj.length));
-    const numOfRows = Math.ceil(arrOfObj.length / numOfCols);
+    const numberOfCols = Math.ceil(Math.sqrt(simulation.totalCoordinates));
+    const numberOfRows = Math.ceil(simulation.totalCoordinates / numberOfCols);
 
     // Generate cells based on the calculated number of columns and rows
-    const cells = arrOfObj.map((obj: number) => (
-        <div key={obj} style={{ backgroundColor: `#${Math.random().toString(16).substr(-6)}`}} className="item">{obj}</div>
-    ));
+    const cells = array.map((index: number) => {
+        const xCell = index % numberOfCols;
+        const yCell = Math.floor(index / numberOfCols);
+        const roverAtCell = simulation.roverList.find(rover => rover.roverXPosition === xCell && rover.roverYPosition === yCell);
+
+        return (
+            <div key={index} className="item" style={{backgroundColor: roverAtCell ? '#000000' : `#${Math.random().toString(16).substr(-6)}`, opacity:"20", display:"flex", justifyContent:"center", alignItems:"center"}}>
+                {roverAtCell &&
+                    <div className="rover" style={{fontSize: 40, color:"white", textAlign:"center"}}>
+                        {roverAtCell.roverName}
+                    </div>
+                }
+                {!roverAtCell &&
+                    <div className="rover" style={{fontSize: 40, color:"white", textAlign:"center"}}>
+                        {index}
+                    </div>
+                }
+            </div>
+        )
+    });
 
     return (
-        <div className="container" style={{ gridTemplateColumns: `repeat(${numOfCols}, minmax(10rem, 1fr))`, gridTemplateRows: `repeat(${numOfRows}, minmax(10rem, 1fr))` }}>
+        <div className="container" style={{
+            gridTemplateColumns: `repeat(${numberOfCols}, minmax(10rem, 1fr))`,
+            gridTemplateRows: `repeat(${numberOfRows}, minmax(10rem, 1fr))`
+        }}>
             {cells}
         </div>
     );
