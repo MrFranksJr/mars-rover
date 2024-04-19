21d8210c3aec09e7d4bf5347730f7dda548451f6
Franky Jr Blondeel <frankyjr@me.com>
2024-04-19 15:15:57 +0200

    finished map detailing

--- frontend-adapters/react-app/src/main/reactsrc/src/AppComponents/MainApp/LeftPanel/Map/Grid.tsx
+++ frontend-adapters/react-app/src/main/reactsrc/src/AppComponents/MainApp/LeftPanel/Map/Grid.tsx
@@ -3,8 +3,8 @@ import styles from '../../../../styles/Map.module.css';
 import {SimulationContext} from "../../../SimulationContext.tsx";
 import locationArrowIcon from "../../../../assets/icons/location-arrow-solid.svg"
 import heartIcon from "../../../../assets/icons/heart-solid.svg"
-import brokenIcon from "../../../../assets/icons/heart-circle-xmark-solid.svg"
-import activeIcon from "../../../../assets/icons/heart-circle-check-solid.svg"
+import brokenIcon from "../../../../assets/icons/circle-xmark-solid2.svg"
+import activeIcon from "../../../../assets/icons/circle-check-solid.svg"
 import compassIcon from "../../../../assets/icons/compass-solid.svg"
 
 const Grid: React.FC = () => {
@@ -17,27 +17,53 @@ const Grid: React.FC = () => {
 
     const cells = array.map((index: number) => {
         const xCell = index % numberOfCols;
-        const yCell = Math.floor(index / numberOfCols);
-        const roverAtCell = simulation.roverList.find(rover => rover.roverXPosition === xCell && rover.roverYPosition === yCell);
+        const yCell = numberOfRows - 1 - Math.floor(index / numberOfCols);
+        const roversAtCell = simulation.roverList.filter(rover => rover.roverXPosition === xCell && rover.roverYPosition === yCell);
 
         return (
             <div key={index} className="item" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                {roverAtCell &&
-                    <div className={styles.cell} style={{color: "yellow", fontWeight: "bold"}}>
-                        {roverAtCell.roverName}
+                {roversAtCell.length === 1 &&
+                    <div className={styles.cell} style={{fontWeight: "bold"}}>
+                        {roversAtCell[0].roverName}
                         <span className={styles.tooltiptext}>
-                            <div className={styles.roverStatusInfoBlock}><img src={locationArrowIcon} className={styles.mapIcon}/><div style={{textWrap:"nowrap"}}>{xCell} : {yCell}</div></div>
-                            <div className={styles.roverStatusInfoBlock}><img src={heartIcon} className={styles.mapIcon}/> {roverAtCell.hitPoints}/5</div>
-                            <div className={styles.roverStatusInfoBlock}><img src={compassIcon} className={styles.mapIcon}/> {roverAtCell.roverHeading}</div>
-                            <div className={styles.roverStatusInfoBlock}><img src={activeIcon} className={styles.mapIcon}/> {roverAtCell.operationalStatus}</div>
+                            <div className={styles.roverStatusInfoBlock}><img src={locationArrowIcon} className={styles.locationIcon} alt="location icon"/><div style={{textWrap:"nowrap"}}>{xCell} – {yCell}</div></div>
+                            <div className={styles.roverStatusInfoBlock}><img src={compassIcon} className={styles.compassIcon} alt="compass icon"/> {roversAtCell[0].roverHeading}</div>
+                            <div className={styles.roverStatusInfoBlock}><img src={heartIcon} className={styles.healthIcon} alt="heart icon"/> {roversAtCell[0].hitPoints}/5</div>
+                            {roversAtCell[0].operationalStatus === "OPERATIONAL" &&
+                                <div className={styles.roverStatusInfoBlock} style={{color:"7ecc2d"}}><img src={activeIcon} className={styles.goodIcon} alt="good status icon"/> {roversAtCell[0].operationalStatus}</div>
+                            }
+                            {roversAtCell[0].operationalStatus === "BROKEN" &&
+                                <div className={styles.roverStatusInfoBlock} style={{color:"f18b82"}}><img src={brokenIcon} className={styles.brokenIcon} alt="bad status icon"/> {roversAtCell[0].operationalStatus}</div>
+                            }
                         </span>
                     </div>
                 }
-                {!roverAtCell &&
+                {roversAtCell.length > 1 &&
+                    <div className={styles.cell} style={{ fontWeight: "bold" }}>
+                        {roversAtCell.length}
+                        <span className={styles.tooltiptext} style={{display:"flex", gap:30}}>
+                            {roversAtCell.map((rover, roverIndex) => (
+                                <div key={roverIndex}>
+                                    <div className={styles.roverStatusInfoBlock} style={{fontWeight:"bold"}}>{rover.roverName}</div>
+                                    <div className={styles.roverStatusInfoBlock}><img src={locationArrowIcon} className={styles.locationIcon} alt="location icon"/><div style={{textWrap:"nowrap"}}>{xCell} – {yCell}</div></div>
+                                    <div className={styles.roverStatusInfoBlock}><img src={compassIcon} className={styles.compassIcon} alt="compass icon"/> {roversAtCell[0].roverHeading}</div>
+                                    <div className={styles.roverStatusInfoBlock}><img src={heartIcon} className={styles.healthIcon} alt="heart icon"/> {roversAtCell[0].hitPoints}/5</div>
+                                    {roversAtCell[0].operationalStatus === "OPERATIONAL" &&
+                                        <div className={styles.roverStatusInfoBlock} style={{color:"7ecc2d"}}><img src={activeIcon} className={styles.goodIcon} alt="good status icon"/> {roversAtCell[0].operationalStatus}</div>
+                                    }
+                                    {roversAtCell[0].operationalStatus === "BROKEN" &&
+                                        <div className={styles.roverStatusInfoBlock} style={{color:"f18b82"}}><img src={brokenIcon} className={styles.brokenIcon} alt="bad status icon"/> {roversAtCell[0].operationalStatus}</div>
+                                    }
+                                </div>
+                            ))}
+                        </span>
+                    </div>
+                }
+                {!roversAtCell[0] &&
                     <div className={styles.cell}>
 
                         <span className={styles.tooltiptext}>
-                            <div className={styles.roverStatusInfoBlock}><img src={locationArrowIcon} className={styles.mapIcon}/><div style={{textWrap:"nowrap"}}>{xCell} : {yCell}</div></div>
+                            <div className={styles.roverStatusInfoBlock}><img src={locationArrowIcon} className={styles.locationIcon} alt="location icon"/><div style={{textWrap: "nowrap"}}>{xCell} – {yCell}</div></div>
                         </span>
                     </div>
                 }
